48a593b2821f7b49ffb22ab0095b30cf4a8f327e
Andre Demavivas <andredemavivas@gmail.com>
2025-12-05 18:21:31 +0800

    chore: added route in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
+import agentRoutes from "./routes/agent.routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/agents", agentRoutes);
 
 app.listen(3000, () => console.log("API running on http://localhost:3000"));
